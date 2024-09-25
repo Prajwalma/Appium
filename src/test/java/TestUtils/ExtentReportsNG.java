c1ac7b865afbfa2847f1e95e7eb89391a8c692ea
prajwal <prajwalma44@gmail.com>
2024-09-25 12:22:43 +0530

    extent reports integration with jenkins

--- src/test/java/TestUtils/ExtentReportsNG.java
+++ src/test/java/TestUtils/ExtentReportsNG.java
@@ -3,13 +3,16 @@ package TestUtils;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
+import java.util.Properties;
+
 public class ExtentReportsNG {
     static ExtentReports extent;
 
     public static ExtentReports getreportr()
     {
-        String path = System.getProperty("user.dir")+"//reports//report.html";;
-
+        Properties prop = new Properties();
+       // String path = System.getProperty("user.dir")+"//reports//report.html";;
+        String path = prop.getProperty("serverreportpath");
         ExtentSparkReporter reporter = new ExtentSparkReporter(path);
         reporter.config().setReportName("Appium Automation Report");
         reporter.config().setDocumentTitle("Appium Automation Results");
